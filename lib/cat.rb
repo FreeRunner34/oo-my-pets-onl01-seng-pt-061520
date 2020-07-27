a5bd8fa16bb94e7210b463f319fc7562eb677d0f
Nikolaus Riggi <nikolaus.riggi@gmail.com>
2020-07-27 05:47:31 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,21 @@
 class Cat
-  # code goes here
+  attr_reader :name
+  attr_accessor :mood
+  
+  @@all = []
+  
+  def initialize(name, mood = "nervous")
+    @name = name
+    @mood = "nervous"
+    save 
+  end
+
+  def self.all
+  @@all
+  end
+
+  def save 
+  @@all << self 
+  end 
+
 end
